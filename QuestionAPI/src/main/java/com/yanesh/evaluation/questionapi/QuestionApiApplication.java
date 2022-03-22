51474e28d174c1ca1d462aecebe04b6ed41874b1
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-03-22 15:42:56 +0400

    Change questionApi to eureka client

--- QuestionAPI/src/main/java/com/yanesh/evaluation/questionapi/QuestionApiApplication.java
+++ QuestionAPI/src/main/java/com/yanesh/evaluation/questionapi/QuestionApiApplication.java
@@ -2,7 +2,9 @@ package com.yanesh.evaluation.questionapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class QuestionApiApplication {
 
