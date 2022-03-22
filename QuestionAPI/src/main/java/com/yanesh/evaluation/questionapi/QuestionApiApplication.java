6611242f6d2f283075e60917cc1f7c40e006c387
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-03-22 12:33:24 +0400

    Initial commit

---
+++ QuestionAPI/src/main/java/com/yanesh/evaluation/questionapi/QuestionApiApplication.java
@@ -0,0 +1,13 @@
+package com.yanesh.evaluation.questionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionApiApplication.class, args);
+    }
+
+}
