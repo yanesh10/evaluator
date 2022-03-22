ae65f1524222ba5233e59bb4192505b6f859dbd3
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-03-22 09:12:22 +0400

    Initial commit

---
+++ common-library/src/main/java/com/yanesh/evaluator/commonlibrary/CommonLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.yanesh.evaluator.commonlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLibraryApplication.class, args);
+    }
+
+}
---
+++ common-library/src/test/java/com/yanesh/evaluator/commonlibrary/CommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanesh.evaluator.commonlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
