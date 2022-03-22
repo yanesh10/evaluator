4f58e60e4ae5a342e8d349be5a71c054af2fd07e
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-03-22 09:26:25 +0400

    Initial commit

---
+++ QuestionETL/src/main/java/com/yanesh/evaluator/questionetl/QuestionEtlApplication.java
@@ -0,0 +1,13 @@
+package com.yanesh.evaluator.questionetl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionEtlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionEtlApplication.class, args);
+    }
+
+}
---
+++ QuestionETL/src/test/java/com/yanesh/evaluator/questionetl/QuestionEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanesh.evaluator.questionetl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionEtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
