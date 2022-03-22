b43354ee0c8cbce0baf3a21219c4df657f079f25
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-03-22 15:22:20 +0400

    Update Library

--- common-library/src/main/java/com/yanesh/evaluator/commonlibrary/models/Question.java
+++ common-library/src/main/java/com/yanesh/evaluator/commonlibrary/models/Question.java
@@ -1,16 +1,21 @@
 package com.yanesh.evaluator.commonlibrary.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Document
+@Builder
+@Getter
 public class Question {
 
     private String question;
     private List<Answer> possibleAnswers;
     private Answer validAnswer;
+    private List<String> tags;
 
 }
