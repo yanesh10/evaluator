9bde2db7ea706e8fa95a4a9f116b826f8cf01852
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-03-30 15:43:41 +0400

    Add User Storage SPI provider

---
+++ RemoteUserStorageProvider/src/main/java/com/evaluator/remoteuserstorageprovider/RemoteUserStorageProviderApplication.java
@@ -0,0 +1,13 @@
+package com.evaluator.remoteuserstorageprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteUserStorageProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteUserStorageProviderApplication.class, args);
+    }
+
+}
---
+++ RemoteUserStorageProvider/src/test/java/com/evaluator/remoteuserstorageprovider/RemoteUserStorageProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluator.remoteuserstorageprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteUserStorageProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
